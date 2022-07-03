b39665326a06729784b599c88a52fb83e67f8018
sunny shah <107214926+sshah1899@users.noreply.github.com>
2022-07-03 16:56:22 +0530

    Assignment of JavaScript Strings and Operators
    1.Create a variable add and assign it the sum of a and b.
    2.Create a variable sub and assign it result of b subtracted from a.
    3.Create a variable mul and assign it the product of a and b.
    4.Create a variable div and assign it the result of of a divided by b.
    5.Create a variable inc and assign it the preincremented value of a.
    6.Create a variable dec and assign it the predecremented value of b.
    7.Console log the length of string.
    8.Extract "AlmaBetter" from string using slice() and console log it.
    9.Replace "Javascript" with "Web3.0" in test and console log it.
    10.Convert test to upper case using built in function and console log it.
    11.Concat "Web3.0 is future" to test and console log it.

---
+++ JavaScript Strings and Operators.js
@@ -0,0 +1,49 @@
+/* Write your code here */
+let a, b
+a = 5;
+b= 10
+
+//Create a variable add and assign it the sum of a and b.
+result = a + b;
+console.log(result);
+
+//Create a variable sub and assign it result of b subtracted from a.
+result1 = b-a;
+console.log(result1);
+
+//Create a variable mul and assign it the product of a and b.
+result2 = a*b;
+console.log(result2);
+
+//Create a variable div and assign it the result of of a divided by b.
+result3 = a/b;
+console.log(result3);
+
+//Create a variable inc and assign it the preincremented value of a.
+result4 = ++a;
+console.log(result4);
+
+//Create a variable dec and assign it the predecremented value of b.
+result5 = --b;
+console.log(result5);
+
+
+let sam = "Learning Javascript with AlmaBetter";
+test1 =  "Web3.0 is future";
+
+//Console log the length of string
+console.log(sam.length);
+
+//Extract "AlmaBetter" from string using slice() and console log it
+console.log(sam.slice(25));
+
+//Replace "Javascript" with "Web3.0" in test and console log it
+console.log(sam.replace('Javascript', 'Web3.0'));
+
+//Convert test to upper case using built in function and console log it
+console.log(sam.toUpperCase());
+
+//Concat "Web3.0 is future" to test and console log it
+console.log(sam.concat(test1));
+
+
